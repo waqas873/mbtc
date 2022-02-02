a2943e12daf32e907a53ba85c243099e0e856274
waqas873 <50407015+waqas873@users.noreply.github.com>
2022-02-02 13:23:43 +0500

    work

--- application/controllers/Team_purchases.php
+++ application/controllers/Team_purchases.php
@@ -137,12 +137,6 @@ class Team_purchases extends CI_Controller
         	// 	$wallet_balance = $wallet_balance+0.2;
         	// }
 
-        	if($package_with_fees>$wallet_balance){
-        		$balance_need = $package_with_fees-$wallet_balance;
-                $this->session->set_flashdata('error_message', "Please transfer $".$balance_need." more to your wallet to purchase this package including $".$package_fees." Fee.");
-		        redirect('team_purchases'); exit;
-        	}
-
     		$data = [];
     		$data['up_package_amount'] = $formData['up_package_amount'];
     		$data['up_user_id'] = $user_id;
@@ -152,13 +146,15 @@ class Team_purchases extends CI_Controller
     		$where = 'up_user_id = "'.$user_id.'"';
             $result = $this->user_packages->get_where('*', $where, true, '', '', '');
             if(!empty($result)){
-        	   	$this->session->set_flashdata('error_message', 'This user already have purchased a package.');
+                $this->process_update($formData);
+            }
+
+            if($package_with_fees>$wallet_balance){
+                $balance_need = $package_with_fees-$wallet_balance;
+                $this->session->set_flashdata('error_message', "Please transfer $".$balance_need." more to your wallet to purchase this package including $".$package_fees." Fee.");
                 redirect('team_purchases'); exit;
             }
-            // if($up_package_amount>4499){
-        	   // 	$this->session->set_flashdata('error_message', 'You cannot purchase a package more than $1000.');
-               //  redirect('user/user_dashboard'); exit;
-            // }
+
             $this->db->trans_start();
         	$up_id = $this->user_packages->save($data);
         	$tp = [];
@@ -244,5 +240,135 @@ class Team_purchases extends CI_Controller
 	        redirect('team_purchases');
     	}
 	}
+
+    public function process_update($formData = [])
+    {   
+        if ($this->session->userdata("role") == "admin") {
+            redirect("User/UserDashboard"); exit;
+        }
+        $data = [];
+        if(empty($formData)){
+            $this->session->set_flashdata('error_message', 'Invalid request to upgrade the package.');
+            redirect('team_purchases');
+        }
+
+        $logged_in_user = $this->session->userdata('id');
+
+        $user_id = $formData['id'];
+        $package_id = $formData['package_id'];
+        $up_package_amount = $formData['up_package_amount'];
+
+        $pkg_detail = package_detail($user_id);
+        if(empty($pkg_detail)){
+            $this->session->set_flashdata('error_message', 'Please first activate your desired package than you can upgrade your package.');
+            redirect('team_purchases'); exit;
+        }
+
+        if($up_package_amount <= $pkg_detail['up_package_amount']){
+            $this->session->set_flashdata('error_message', 'To upgrade your package new amount must be greater than previous package amount.');
+            redirect('team_purchases');
+            exit;
+        }
+        $where = 'package_id = "'.$package_id.'"';
+        $result = $this->packages->get_where('*', $where, true, '', '', '');
+        $package_min_amount = $result[0]['package_min_amount'];
+        $package_max_amount = $result[0]['package_max_amount'];
+        if( $up_package_amount > $package_max_amount || $up_package_amount < $package_min_amount ){
+            $this->session->set_flashdata('error_message', "The minimum amount to upgrade this package is $".$package_min_amount." and maximum is $".$package_max_amount.".");
+            redirect('team_purchases');exit;
+        }
+        $amount_update = $up_package_amount-$pkg_detail['up_package_amount'];
+        $wallet_balance = wallet_balance($logged_in_user);
+        if($wallet_balance < $amount_update){
+            $balance_need = $amount_update-$wallet_balance;
+            $this->session->set_flashdata('error_message', "Please transfer $".$balance_need." more to your wallet to upgrade your package.");
+            redirect('team_purchases'); exit;
+        }
+        
+        $this->db->trans_start();
+        $save = [];
+        $save['upu_user_id'] = $user_id;
+        $save['upu_package_id'] = $package_id;
+        $save['upu_package_amount'] = $up_package_amount;
+        $save['upu_previous_amount'] = $pkg_detail['up_package_amount'];
+        $save['upu_upgraded_at'] = date('Y-m-d');
+        $upu_id = $this->user_packages_upgrade->save($save);
+
+        update_wallet_balance($logged_in_user,$amount_update,true);
+
+        $update = [];
+        $update['up_package_id'] = $package_id;
+        $update['up_package_amount'] = $up_package_amount;
+        $update['up_upgraded_at'] = date('Y-m-d');
+        $this->user_packages->update_by('up_user_id',$user_id,$update);
+
+        $tp = [];
+        $tp['purchased_by'] = $logged_in_user;
+        $tp['up_id'] = $pkg_detail['up_id'];
+        $tp['amount'] = $up_package_amount;
+        $tp['type'] = 2;
+        $this->team_purchases->save($tp);
+
+        $where = "user_packages_upgrade.upu_id = '".$upu_id."'";
+        $joins = array(
+            '0' => array('table_name' => 'user_packages user_packages',
+                'join_on' => ' user_packages.up_user_id = user_packages_upgrade.upu_user_id ',
+                'join_type' => 'left'
+            ),
+            '1' => array('table_name' => 'users users',
+                'join_on' => ' users.id = user_packages_upgrade.upu_user_id ',
+                'join_type' => 'left'
+            )
+        );
+        $from_table = "user_packages_upgrade user_packages_upgrade";
+        $select_from_table = 'user_packages_upgrade.*,user_packages.*,users.*';
+        $result = $this->user_packages_upgrade->get_by_join($select_from_table, $from_table, $joins, $where, '','', '', '', '', '', '', '',true);
+        $result = $result[0];
+        //debug($result,true);
+
+        $user_id = $result['id'];
+        $parent_id = (!empty($result['referral_id']))?$result['referral_id']:0;
+        
+        // .......delivering of referal bonus.........
+        $pD = packageDetail($parent_id);
+        if(!empty($pD)){
+            $parent_package_amount = $pD['up_package_amount'];
+            $referal_bonus = ((8/100)*$amount_update);
+            update_user_balance($parent_id,$referal_bonus,false);
+            daily_earnings($parent_id,$referal_bonus,"Referal Bonus",date('Y-m-d'),$user_id);
+        }
+
+        // .......delivering of points to users.........
+        $no_of_points = $amount_update;
+        $user_id = $result['parent_id'];
+        $user_position = $result['position'];
+        $condition = true;
+        if($user_position == "right" || $user_position == "left"){
+            while ($user_id > 1 && $condition === true) {
+                $right_points = ($user_position=="right")?$no_of_points:0;
+                $left_points = ($user_position=="left")?$no_of_points:0;
+                points($user_id,$left_points,$right_points);
+                $where = "id = '".$user_id."'";
+                $result_2 = $this->users->get_where('*', $where, true, '', '', '');
+                if(!empty($result_2)){
+                    $result_2 = $result_2[0];
+                    $user_position = $result_2['position'];
+                    $user_id = $result_2['parent_id'];
+                }
+                else{
+                    $condition = false;
+                }
+            }
+        }
+        $this->db->trans_complete();
+
+        $pkg_detail = package_detail($this->session->userdata('id'));
+        $name = $this->session->userdata('fullname');
+        $message = "Your package has been upgraded successfully to  ".$pkg_detail['package_name']." package.<br/><br/> ";
+        $message .= "Regards,<br/>Administration of MBTC";
+        send_email_to($name,$this->session->userdata('email'),$message,'Package Upgrade');
+        $this->session->set_flashdata('success_message', 'Package has been upgraded successfully.');
+        redirect('team_purchases');
+    }
 	
 }
